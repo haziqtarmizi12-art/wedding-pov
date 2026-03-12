51b41904f3f34f828500ae54c679f7e21de2e438
haziqtarmizi12-art <haziq.tarmizi12@gmail.com>
2026-03-13 00:54:44 +0800

    button2

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       {/* Buttons */}
       <div
         className="absolute w-full flex flex-col items-center gap-4"
-        style={{ top: "45%" }}
+        style={{ top: "47%" }}
       >
 
         {/* Take Photos (Glowing) */}
